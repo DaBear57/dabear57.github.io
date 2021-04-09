6807ddfd0dc7155760b07128a57be39baffb50ed
DaBear57 <ben.r.debear@gmail.com>
2021-04-09 19:23:24 -0400

    newpics

--- gallery.py
+++ gallery.py
@@ -3,7 +3,7 @@ folder = r'C:\Users\bdebe_000\Documents\DeBear_Necessities\dabear57.github.io\as
 files = os.listdir(folder)
 for i in range(len(files)):
     #print(files[i])
-    os.system("rename " + folder+files[i]+"\\ img"+str(i+1)+".jpg")
+    os.system("rename "+folder+"\\"+files[i]+" img"+str(i+1)+".jpg")
 os.system("git add -A")
 os.system("git commit -m newpics")
 os.system("git push")
